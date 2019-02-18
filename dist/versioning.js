69f786d5ae3b54b4021ce49f79bb50917f6edb5d
imdbsd <budisuryadarma0407@gmail.com>
2019-02-18 12:41:33 +0800

    removing dist

--- dist/versioning.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.version = "1.0.0";
-exports.description = "Unofficial CLI tools for checking movie schedule from jadwalnonton.com";
